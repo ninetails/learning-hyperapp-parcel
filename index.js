24f78eb30f62f1b6b4e564f56ea36f83b1b38f7b
Carlos Kazuo <ninetails.nine@gmail.com>
2018-02-05 20:47:23 -0200

    copied code + style

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 import { h, app } from 'hyperapp'
+import debounce from 'debounce-promise'
 
-const view = () =>
-  <div>
-    hello hyperapp
-  </div>
+import './main.styl'
 
-app({}, {}, view, document.body)
+const state = {
+  username: '',
+  userData: null,
+}
+
+const getUserDataFn = username =>
+  fetch(`https://api.github.com/users/${username}`)
+    .then(res => res.json())
+
+const getUserData = debounce(getUserDataFn, 700)
+
+const actions = {
+  updateUsername: (username) => (state, actions) => {
+    getUserData(username).then(actions.setUserData)
+    return { username }
+  },
+  setUserData: userData => state => ({ userData })
+}
+
+const view = (state, actions) =>
+  <main>
+    <div>Search github users:</div>
+    <input
+      type='text'
+      className='searchInput'
+      value={state.username}
+      oninput={e => actions.updateUsername(e.target.value)}
+    />
+    <br />
+    <div className='userCard'>
+      {state.userData ? (
+        <div>
+          <img class='userCard__img' src={state.userData.avatar_url} />
+          <div class='userCard__name'>{state.userData.name}</div>
+          <div class='userCard__location'>{state.userData.location}</div>
+        </div>
+      ) : (
+          <div>search 'em</div>
+        )}
+    </div>
+  </main>
+
+app(state, actions, view, document.body)
